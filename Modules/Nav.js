79923aac02bc17d2a4507733e513ee46927668e6
tylerjusfly <tylerjusfly1@gmail.com>
2022-06-18 09:13:05 +0100

    creating modularization

---
+++ Modules/Nav.js
@@ -0,0 +1,11 @@
+const Nav = () => `
+    <nav>
+        <a href="#" class="logo-name">
+            <img src="./images//shakurLogo.png" alt="tupacShakur">
+        </a>
+        <span class="search-box">
+            <input type="text" placeholder="Search" id="search">
+            <button><i class="fa fa-search"></i></button>
+        </span>
+     </nav>
+`
