c4511b330c59f86749a132b64d499818f100a671
tylerjusfly <tylerjusfly1@gmail.com>
2022-07-04 15:33:50 +0100

    looping through music to display card for each

--- Modules/music.js
+++ Modules/music.js
@@ -1,30 +1,4 @@
-// const MusicCard = () => {
-
-//     return `
-//     <div class="music-card">
-//     <img id="play" src="./images/circle-play-solid.svg" alt="play"> 
-//     <audio class="audio" src=""></audio>
-//     <span class="song-title"> Picture Me Rolling - Tupac </span>
-//     </div>
-//     `
-// }
-// const Library = () => {
-//     return `
-//     <h3>Songs Library</h3>
-//     ${MusicCard()}
-//     ${MusicCard()}
-//     `
-// }
-
-
-
-// document.getElementById("root").innerHTML = Library()
-
 const root = document.getElementById("root")
-// const musicCards = document.querySelectorAll(".music-card")
-// const playbtns = document.querySelectorAll("#play")
-// const songTitle = document.querySelector(".song-title")
-//const audio = document.querySelector(".audio")
 
     // Song title array 
     const songs = ['Picture Me Rolling - Tupac', 'Tupac Shakur Dear Mama', 'Thugz Mansion - Tupac', '2pac ft Outlawz High speed']
@@ -32,7 +6,7 @@ const root = document.getElementById("root")
 
     for(let i = 0; i < songs.length; i++){
         console.log(songs[i])
-        //songTitle.innerText = songs[i]
+
         // Creating music card div
         const musicCard = document.createElement('div')
         musicCard.classList = 'music-card'
@@ -48,16 +22,17 @@ const root = document.getElementById("root")
 
         // inserting Audio Tag
         const audio = document.createElement('audio')
-        const songTitle = document.createElement('span')
-        songTitle.classList = 'song-title'
         audio.classList = 'audio'
-        songTitle.innerText = songs[i]
         audio.src = `../songs/${songs[i]}.mp3`
         musicCard.appendChild(audio)
-        musicCard.appendChild(songTitle)
-        //loadSong(songs[i])
 
-            // Running functions
+        // Inserting span tag to hold song title
+        const songTitle = document.createElement('span')
+        songTitle.classList = 'song-title'
+        songTitle.innerText = songs[i]
+        musicCard.appendChild(songTitle)
+
+            // Running functions to play Songs
         function playSong(){
                     musicCard.classList.add('play')
                     console.log("playin")
@@ -69,17 +44,16 @@ const root = document.getElementById("root")
          }
      
         //  mother function
-            playBtn.addEventListener("click", () => {
-        const isPlaying = musicCard.classList.contains('play')
+        playBtn.addEventListener("click", () => {
+            const isPlaying = musicCard.classList.contains('play')
 
-        if(isPlaying){
-            pauseSong()
-        }
-        else{
-            playSong()
-        }
-
-       })
+            if(isPlaying){
+                pauseSong()
+            }
+            else{
+                playSong()
+            }
+        })
     }
 
 
