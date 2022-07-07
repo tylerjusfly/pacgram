fd53e42cb3ad215ce435301fb9c2f8f3dd7e2644
tylerjusfly <tylerjusfly1@gmail.com>
2022-07-07 09:15:27 +0100

    changing for loops to map

--- Modules/music.js
+++ Modules/music.js
@@ -4,37 +4,39 @@ const root = document.getElementById("root")
     const songs = ['Picture Me Rolling - Tupac', 'Tupac Shakur Dear Mama', 'Thugz Mansion - Tupac', '2pac ft Outlawz High speed', '2Pac RL Hugger Until The End Of Time Letterbox Version']
 
         // Loopng through songs in array
-    for(let i = 0; i < songs.length; i++){
+    songs.map((val ) => {
+        //console.log(index)
+        console.log(val)
 
         // Creating music card div
         const musicCard = document.createElement('div')
         musicCard.classList = 'music-card'
         root.appendChild(musicCard)
 
-        // inserting img tag
+        // // inserting img tag
         const playBtn = document.createElement('img')
         playBtn.setAttribute('id', 'play')
-        //imgTag.classList = 'play'
+        
         playBtn.src = '../images/circle-play-solid.svg'
         playBtn.alt = 'play'
         musicCard.appendChild(playBtn)
 
-        // inserting Audio Tag
+        // // inserting Audio Tag
         const audio = document.createElement('audio')
         audio.classList = 'audio'
-        audio.src = `../songs/${songs[i]}.mp3`
+        audio.src = `../songs/${val}.mp3`
         musicCard.appendChild(audio)
 
-        // Inserting span tag to hold song title
+        // // Inserting span tag to hold song title
         const songTitle = document.createElement('span')
         songTitle.classList = 'song-title'
-        songTitle.innerText = songs[i]
+        songTitle.innerText = val
         musicCard.appendChild(songTitle)
 
             // Running functions to play Songs
         function playSong(){
                     musicCard.classList.add('play')
-                    console.log("playin")
+                    
                     audio.play()
                 }
         function pauseSong(){
@@ -53,5 +55,6 @@ const root = document.getElementById("root")
                 playSong()
             }
         })
-    }
+
+    })
 
